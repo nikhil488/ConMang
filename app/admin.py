d733932df54a2a1068ad8a7f6d17ffba41c33081
nikhil488 <hacktalk272@gmail.com>
2020-05-29 07:33:37 -0800

    first commit

---
+++ app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Contact
+from django.contrib.auth.models import Group
+from import_export.admin import ImportExportModelAdmin
+# Register your models here.
+
+
+class ContactAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name', 'gender', 'email', 'info', 'phone')
+    list_display_links = ('id', 'name')
+    list_editable = ('info',)
+    list_per_page = 10
+    search_fields = ('name', 'gender', 'email', 'info', 'phone')
+    list_filter = ('gender', 'date_added')
+
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.unregister(Group)
